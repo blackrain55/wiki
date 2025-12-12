571f3c482d2ab654e682143942e190c2be43efbe
blackrain55 <64234652+blackrain55@users.noreply.github.com>
2025-12-12 20:36:21 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "TamTam Knowlegde",
+  description: "逻辑学 / 嵌入式Linux / 计算机网络",
+  lang: 'zh-CN',  // 设置语言为中文
+
+  themeConfig: {
+    // 1. 顶部导航栏 (一直显示在最上面)
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '逻辑学', link: '/logic/logic1_2025-12-12' },
+      { text: '嵌入式Linux', link: '/linux/rk3506' },
+      { text: '计算机网络', link: '/network/gfw' }
+    ],
+
+    // 2. 侧边栏 (核心逻辑：进入不同文件夹，显示不同目录)
+    sidebar: {
+      // 当用户在 logic 文件夹下时：
+      '/logic/': [
+        {
+          text: '逻辑学导论',
+          items: [
+            { text: '第一章：逻辑学基本概念', link: '/logic/logic1_2025-12-12' },
+            // 以后在这里加：{ text: '第二章：定义', link: '/logic/chapter2' }
+          ]
+        }
+      ],
+
+      // 当用户在 linux 文件夹下时：
+      '/linux/': [
+        {
+          text: 'RK3506 开发实战',
+          items: [
+            { text: '原理分析', link: '/linux/rk3506' },
+            // 以后在这里加：{ text: '环境搭建', link: '/linux/env-setup' }
+          ]
+        }
+      ],
+
+      // 当用户在 network 文件夹下时：
+      '/network/': [
+        {
+          text: '网络技术研究',
+          items: [
+            { text: 'GFW原理', link: '/network/gfw' }
+          ]
+        }
+      ]
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/blackrain55/' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "TamTam"
+  text: "积累 · 沉淀 · 分享"
+  tagline: Nope
+  actions:
+    - theme: brand
+      text: 🚀甚至可以加个按钮
+      link: /linux/rk3506
+
+features:
+  - title: 🧠 逻辑学导论
+    details: 包含《逻辑学导论》的多篇读书笔记与深度思考。
+    link: /logic/intro
+    icon: 🧠
+  - title: 🐧 嵌入式 Linux
+    details: 基于 RK3506 的原理分析、环境搭建与实战项目。
+    link: /linux/rk3506
+    icon: 🐧
+  - title: 🌐 计算机网络
+    details: 关于网络连通性与 GFW 技术的底层原理研究。
+    link: /network/gfw
+    icon: 🌐
+---
+
